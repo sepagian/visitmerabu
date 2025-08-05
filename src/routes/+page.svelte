b16d2fc8523bd621a2395cda54b43fd8281f1a89
sepagian <rizkydana14@gmail.com>
2025-08-05 11:48:27 +0800

    update the landing page copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,49 +2,49 @@
 	import { Button } from "bits-ui";
 </script>
 
-<section
-	class="relative flex h-screen flex-col items-center justify-center gap-4 bg-[url($lib/assets/hero.jpg)] bg-cover bg-center p-8">
+<section class="section-base items-center bg-[url(/hero.jpg)] bg-cover bg-center p-4">
 	<div
-		class="absolute inset-0 z-0 bg-black opacity-60"
+		class="bg-overlay"
 		aria-hidden="true">
 	</div>
-	<div class="z-10 max-w-2xl">
-		<h1 class="prose text-wrap text-center font-sans text-4xl font-bold text-amber-400">
+	<div class="section-container">
+		<h1 class="heading-h1 text-merabuAmber-400 text-center">
 			Menjejak di Tanah Merabu—Kampung di Pelukan Hutan Kalimantan
 		</h1>
 	</div>
-	<div class="z-10 max-w-2xl">
-		<p class="prose font-noto text-wrap text-center text-amber-300">
+	<div class="section-container">
+		<p class="paragraph-base text-merabuAmber-300 text-center">
 			Di balik perbukitan batu kapur dan aliran Sungai Lesan, Kampung Merabu berdiri sebagai
 			penjaga alam dan budaya Dayak. Jelajahi tempat di mana waktu terasa melambat, dan
 			kehidupan berjalan selaras dengan bumi.
 		</p>
 	</div>
 </section>
-<section class="flex h-screen flex-col items-start justify-center gap-4 bg-amber-50 p-4 sm:px-12">
-	<span class="font-bold text-sky-600">Kampung kecil dengan semangat besar</span>
-	<div class="max-w-2xl">
-		<h1 class="prose text-wrap text-left font-sans text-4xl font-bold text-sky-800">
+<section class="section-base bg-merabuAmber-50">
+	<span class="paragraph-caption text-merabuSky-600">Kampung kecil dengan semangat besar</span>
+	<div class="section-container">
+		<h1 class="heading-h1 text-merabuSky-800">
 			Kampung Merabu: Hidup Bersama Alam, Bukan Merampas Alam
 		</h1>
 	</div>
-	<div class="max-w-4xl">
-		<p class="prose font-noto text-wrap text-left text-sky-700">
+	<div class="section-container">
+		<p class="paragraph-base text-merabuSky-700">
 			Di Merabu, hutan bukan objek wisata—ia adalah rumah, guru, dan sumber kehidupan. Warga
 			tidak datang untuk menguasai, tapi lahir dari tanah yang sama dan tumbuh bersama alam
 			yang dipelihara turun-temurun.
 		</p>
-		<p class="prose font-noto text-wrap text-left text-sky-700">—</p>
-		<p class="prose font-noto text-wrap text-left text-sky-700">
-			Setiap jalur pendakian, setiap sungai, setiap kisah yang dibisikkan di bawah pohon
-			tua—adalah bagian dari hubungan yang dijaga dengan penuh hormat. Di sini, alam tidak
-			dikorbankan demi kunjungan, melainkan dihadirkan sebagai teman perjalanan.
+		<p class="text-merabuSky-700 mobile-invisible text-base">—</p>
+		<p class="paragraph-base text-merabuSky-700 mobile-invisible">
+			Setiap jalur pendakian, setiap sungai, setiap kisah yang diwariskan—adalah bagian dari
+			hubungan yang dijaga dengan penuh hormat. Di sini, alam tidak dikorbankan demi
+			kunjungan, melainkan dihadirkan sebagai teman perjalanan.
 		</p>
-		<p class="prose font-noto text-wrap text-left text-sky-700">—</p>
-		<p class="prose font-noto text-wrap text-left text-sky-700">
+		<p class="text-merabuSky-700 mobile-invisible text-base">—</p>
+		<p class="paragraph-base text-merabuSky-700 mobile-invisible">
 			Kampung Merabu membuka diri bukan untuk dieksploitasi, tetapi untuk dipahami. Bagi
 			mereka yang datang dengan hati terbuka, kampung ini bukan sekadar tempat
 			singgah—melainkan cermin cara hidup yang lebih bijak, lebih tenang, dan lebih bermakna.
 		</p>
 	</div>
+	<button class="btn-base btn-primary desktop-invisible">Pelajari Lebih Lanjut</button>
 </section>
