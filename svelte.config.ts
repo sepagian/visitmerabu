e8ca708fda5a160439cae8583eb6f63635e21270
sepagian <rizkydana14@gmail.com>
2025-08-05 11:56:14 +0800

    updated config to ts

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,11 +1,10 @@
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import UnoCSS from "@unocss/svelte-scoped/preprocess";
+import type { Config } from "@sveltejs/kit";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
+const config: Config = {
 	preprocess: [vitePreprocess(), UnoCSS()],
 	kit: { adapter: adapter() },
 };
