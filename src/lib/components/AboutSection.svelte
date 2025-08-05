d7174999bf275fbb808fc2fefa2b74a50eab558f
sepagian <rizkydana14@gmail.com>
2025-08-05 16:26:05 +0800

    created modular component

---
+++ src/lib/components/AboutSection.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Button } from "bits-ui";
+
+	let { title, subtitle, contentParagraph, buttonText, buttonLink, bgColor } = $props<{
+		title: string;
+		subtitle: string;
+		contentParagraph: string[];
+		buttonText: string;
+		buttonLink: string;
+	}>();
+</script>
+
+<section class="section-base bg-merabuAmber-50">
+	<span class="paragraph-caption text-merabuSky-600">{subtitle}</span>
+	<div class="section-container">
+		<h1 class="heading-h1 text-merabuSky-800">
+			{title}
+		</h1>
+	</div>
+	<div class="section-container">
+		{#each contentParagraph as paragraph}
+			<p class="paragraph-base text-merabuSky-700">
+				{paragraph}
+			</p>
+			{#if paragraph !== contentParagraph[contentParagraph.length - 1]}
+				<p class="text-prose text-base-lg text-merabuSky-700 text-left">—</p>
+			{/if}
+		{/each}
+	</div>
+	<Button.Root
+		class="btn-base bg-merabuSky-700 btn-primary desktop-invisible text-center"
+		href={buttonLink}>{buttonText}</Button.Root>
+</section>
---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { Button } from "bits-ui";
+
+	let { title, description, buttonText, buttonLink, bgImage } = $props<{
+		title: string;
+		description: string;
+		buttonText: string;
+		buttonLink: string;
+		bgImage: string;
+	}>();
+</script>
+
+<section
+	class="section-base items-center bg-cover bg-center p-4"
+	style="background-image: url('{bgImage}')">
+	<div
+		class="bg-overlay"
+		aria-hidden="true">
+	</div>
+	<div class="section-container">
+		<h1 class="heading-h1 text-merabuAmber-400 text-center">
+			{title}
+		</h1>
+	</div>
+	<div class="section-container">
+		<p class="paragraph-base text-merabuAmber-300 text-center">
+			{description}
+		</p>
+	</div>
+	{#if buttonText && buttonLink}
+		<Button.Root
+			href={buttonLink}
+			class="btn-base btn-primary z-10">{buttonText}</Button.Root>
+	{/if}
+</section>
