ed84bfcf0dce84dc350a959daf5d1a27294128f9
sepagian <rizkydana14@gmail.com>
2025-08-08 18:15:24 +0800

    modified main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
 	import HeroSection from "$lib/components/HeroSection.svelte";
 	import AboutSection from "$lib/components/AboutSection.svelte";
 	import WhySection from "$lib/components/WhySection.svelte";
+	import ExperienceSection from "$lib/components/ExperienceSection.svelte";
 </script>
 
 <HeroSection />
 <AboutSection />
 <WhySection />
+<ExperienceSection />
