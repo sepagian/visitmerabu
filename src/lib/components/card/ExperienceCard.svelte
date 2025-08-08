7d448dd90795d3de4401c3e2afbebd8817b7de2d
sepagian <rizkydana14@gmail.com>
2025-08-08 18:13:58 +0800

    added new section component

---
+++ src/lib/components/card/ExperienceCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export const {
+		title,
+		imageUrl,
+		span,
+	}: {
+		title: string;
+		imageUrl: string;
+		span?: string;
+	} = $props();
+</script>
+
+<div class="overflow-hidden rounded-2xl shadow-sm {span}">
+	<img
+		src={imageUrl}
+		alt={title}
+		class="h-full w-full object-cover transition-transform duration-300 hover:scale-105"
+		loading="lazy" />
+</div>
