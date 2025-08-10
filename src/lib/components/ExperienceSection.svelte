b889931973379162420e47193f4c8ba831106c1f
sepagian <rizkydana14@gmail.com>
2025-08-10 16:00:05 +0800

    modified the component

--- src/lib/components/ExperienceSection.svelte
+++ src/lib/components/ExperienceSection.svelte
@@ -7,20 +7,29 @@
 <section class="section-base bg-merabuAmber-50 h-fit items-center">
 	<div class="section-container mx-auto max-w-7xl">
 		<!-- Section Header -->
-		<p class="paragraph-caption text-merabuForest-700 text-center">Ada apa di Merabu?</p>
+		<p class="paragraph-caption text-merabuForest-700 text-center">
+			Ada apa di Merabu?
+		</p>
 		<h2 class="heading-h1 text-merabuForest-900 text-center">
 			Setiap Langkah adalah Cerita, Setiap Sudut adalah Pengalaman
 		</h2>
 
 		<!-- Masonry Layout -->
 		<div class="mt-12 columns-1 gap-6 [column-fill:_balance] sm:columns-3 lg:columns-3">
-			{#each experienceImages as exp (exp.title)}
-				<div class="mb-6 break-inside-avoid">
-					<ExperienceCard
-						title={exp.title}
-						imageUrl={exp.imageUrl}
-						span={exp.span} />
-				</div>
+			{#each experienceImages as exp, i (exp.title)}
+				{#if i <= 2 }
+					<div class="mb-6 break-inside-avoid">
+						<ExperienceCard
+							title={exp.title}
+							imageUrl={exp.imageUrl}/>
+					</div>
+				{:else}
+					<div class="mb-6 break-inside-avoid mobile-invisible">
+						<ExperienceCard
+							title={exp.title}
+							imageUrl={exp.imageUrl}/>
+					</div>
+				{/if}	
 			{/each}
 		</div>
 
