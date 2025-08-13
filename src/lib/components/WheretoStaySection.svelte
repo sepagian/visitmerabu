cba35b1dc1d2492fb3d083a35467bc9bb9c454b6
sepagian <rizkydana14@gmail.com>
2025-08-13 10:51:50 +0800

    modified Contact Us section

--- src/lib/components/WheretoStaySection.svelte
+++ src/lib/components/WheretoStaySection.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
-import {whereData} from "$lib/data/landingData";
+	import { whereData } from "$lib/data/landingData";
+	import BookingForm from "./form/BookingForm.svelte";
 </script>
 
-<section class="section-base bg-merabuAmber-50 h-screen items-start">
-	<span class="paragraph-caption {whereData.subtitleColor}">{whereData.subtitle}</span>
-	<div class="section-container">
-		<h1 class="heading-h1 {whereData.titleColor}">
-			{whereData.title}
-		</h1>
-	</div>
-	<div class="section-container">
-        <p class="paragraph-base {whereData.descriptionColor}">{whereData.description}</p>
+<section
+	class="bg-merabuAmber-50 flex h-fit items-center justify-center p-4 sm:p-12"
+	id="contact">
+	<div class="bg-merabuForest-800 h-fit justify-around gap-8 rounded-2xl p-8 sm:flex sm:p-12">
+		<div class="section-container max-w-md pt-8">
+			<span class="paragraph-caption {whereData.subtitleColor}">{whereData.subtitle}</span>
+			<div class="section-container">
+				<h1 class="heading-h1 {whereData.titleColor}">
+					{whereData.title}
+				</h1>
+			</div>
+			<div class="section-container">
+				{#each whereData.description as description}
+					<p class="paragraph-base {whereData.descriptionColor}">{description}</p>
+				{/each}
+			</div>
+		</div>
+		<div class="section-container pt-8">
+			<div class="bg-merabuForest-800">
+				<BookingForm />
+			</div>
+		</div>
 	</div>
 </section>
